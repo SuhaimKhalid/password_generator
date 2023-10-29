8f6070a3ba6b9bb336c5d46f7921fd8d13c0e521
Suhaim Khalid <suhaimkhalid007@gmail.com>
2023-10-29 13:58:48 +0000

    Complete generate password function

--- Assets/script.js
+++ Assets/script.js
@@ -90,6 +90,23 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+  // Declare vaiables to store password options
+  var lowercase_input = confirm("Do you want Lowercase character in Password?");
+  var uppercase_input = confirm("Do you want Uppercase character in Password?");
+  var numeric_input = confirm("Do you want Numeric character in Password?");
+  var special_characters_input = confirm("Do you want Special characters in Password?");
+
+  // Delcare a variable to store array of password option
+  var password_option =[];
+  if(lowercase_input)
+  {
+    password_option = password_option.concat(lowerCasedCharacters);
+  }
+  if(upperCasedCharacters)
+  {
+    password_option = password_option.concat(upperCasedCharacters);
+  }
+  if(num)
 
 }
 
@@ -100,6 +117,36 @@ function getRandom(arr) {
 
 // Function to generate password with user input
 function generatePassword() {
+
+  // Declare a variable to store Length of password from user input
+var Pass = prompt("How Long Password Should Be ?");
+
+// Convert string in to number
+var password_number = parsent(Pass);
+
+// Check if password is number or not
+if(isNaN ==password_number)
+{
+  alert("Please Enter a Number");
+}
+else{
+  // Password length should be greater than 8
+  if(password_number<8)
+  {
+    alert("Password Must be Greater than 8 Character");
+  }
+  // Password length should be Les than 128
+  else if(password_number>128)
+  {
+    alert("Password Must be Less than 128");
+  }
+  else
+  {
+    // Pass the password length number to random function
+    return getRandom(password_number);
+  }
+
+}
 
 }
 
